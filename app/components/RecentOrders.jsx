c3fdd4932b2e9fbcf5b613af258cc4a8f5164533
theisolatedmonk <96rahulseth@gmail.com>
2023-08-26 23:20:39 +0530

    recent order ui created

--- app/components/RecentOrders.jsx
+++ app/components/RecentOrders.jsx
@@ -1,24 +1,30 @@
 'use client'
 
 import React from 'react'
-import data from '@/app/data/data'
-import { FaShoppingBag } from 'react-icons'
-export default function RecentOrders () {
+import { data } from '@/app/data/data'
+import { FaShoppingBag } from 'react-icons/fa'
+
+export function RecentOrders () {
   return (
     <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll'>
       <h1>Recent Orders</h1>
-      {/* <ul>
-        {data.map((order, id) => (
+      <ul>
+        {data.map((order, index) => (
           <li
-            key={id}
+            key={index}
             className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'
           >
             <div className='bg-purple-100 rounded-lg p-3'>
               <FaShoppingBag className='text-purple-800' />
             </div>
+            <div className='pl-4'>
+              <p className='text-gray-800 font-bold'>${order.total} </p>
+              <p className=' text-gray-400 text-sm'>${order.name.first} </p>
+            </div>
+            <p className='lg:flex md:hidden absolute right-6'> ${order.date}</p>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Header from '@/app/components/Header'
 import Sidebar from '@/app/components/Sidebar'
 import TopCards from '@/app/components/TopCards'
 import BarChart from '@/app/components/BarChart'
-import RecentOrders from '@/app/components/RecentOrders'
+import { RecentOrders } from '@/app/components/RecentOrders'
 export default function Home () {
   return (
     <main className='bg-gray-300 min-h-screen'>
