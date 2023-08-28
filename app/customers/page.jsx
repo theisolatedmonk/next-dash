4e680520b8dc6838cde9488bb392fdd135cc3862
theisolatedmonk <96rahulseth@gmail.com>
2023-08-28 19:01:18 +0530

    .

--- app/customers/page.jsx
+++ app/customers/page.jsx
@@ -21,7 +21,7 @@ export default function page () {
             {data.map((order, id) => (
               <li
                 key={id}
-                className='bg-gray-50 hover:bg-gray-100  rounded-lg my-3 p-2 grid md:grid-cols-4  sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer gap-10 md:gap-0 lg:gap-0'
+                className='bg-gray-50 hover:bg-gray-100  rounded-lg my-3 p-2 grid md:grid-cols-4  sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer '
               >
                 <div className='flex items-center '>
                   <div className='bg-purple-100 p-3 rounded-lg'>
